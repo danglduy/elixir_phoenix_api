9cc8f63162655a3dec7702355b92667f61573611
danglduy <38782010+danglduy@users.noreply.github.com>
2019-04-19 21:03:39 +0700

    add jwt authorization

--- lib/myApi_web/router.ex
+++ lib/myApi_web/router.ex
@@ -1,8 +1,14 @@
 defmodule MyApiWeb.Router do
   use MyApiWeb, :router
 
+  alias MyApi.Guardian
+
   pipeline :api do
     plug :accepts, ["json"]
+  end
+
+  pipeline :jwt_authenticated do
+    plug Guardian.AuthPipeline
   end
 
   scope "/api/v1", MyApiWeb do
@@ -10,5 +16,11 @@ defmodule MyApiWeb.Router do
 
     post "sign_up", UserController, :create
     post "sign_in", UserController, :sign_in
+  end
+
+  scope "/api/v1", MyApiWeb do
+    pipe_through [:api, :jwt_authenticated]
+
+    get "/my_user", UserController, :show
   end
 end
